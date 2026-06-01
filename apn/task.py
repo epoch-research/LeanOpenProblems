29b6be5703aa8cf79d1bd50218d9d28fcb4d3e1f
tadamcz <tadamczewskipublic@gmail.com>
2026-06-01 20:19:03 +0100

    fix AI slop

--- apn/task.py
+++ apn/task.py
@@ -117,13 +117,13 @@ services:
     image: {IMAGE_REPOSITORY}:{agent_tag}
     init: true
     entrypoint: tail -f /dev/null
-    mem_limit: 15Gi
+    mem_limit: 15g
     network_mode: none
   scorer:
     image: {IMAGE_REPOSITORY}:{scorer_tag}
     init: true
     entrypoint: tail -f /dev/null
-    mem_limit: 15Gi
+    mem_limit: 15g
     network_mode: none
 """
 
